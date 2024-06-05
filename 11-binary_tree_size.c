15508dc4eaba6970a85ee83546b3ffc36eb5fd21
Christiana Apinoko <christyapinoko@gmail.com>
2024-06-05 07:16:12 -0700

    fix betty error

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,13 +6,13 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    /*recursively compute the size of the left and right subtrees*/
-    size_t left = binary_tree_size(tree->left);
-    size_t right = binary_tree_size(tree->right);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	/*recursively compute the size of the left and right subtrees*/
+	size_t left = binary_tree_size(tree->left);
+	size_t right = binary_tree_size(tree->right);
 
-    return (1 + left + right);
+	return (1 + left + right);
 }
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,15 +6,16 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-    size_t left = binary_tree_leaves(tree->left);
-    size_t right = binary_tree_leaves(tree->right);
-    return (right + left);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	size_t left = binary_tree_leaves(tree->left);
+	size_t right = binary_tree_leaves(tree->right);
+
+	return (right + left);
 }
