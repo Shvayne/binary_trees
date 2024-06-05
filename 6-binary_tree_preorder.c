9dfc1e60b0339b9c293e565f35d71e70a23fc07f
Christiana Apinoko <christyapinoko@gmail.com>
2024-06-05 05:42:50 -0700

    fix betty errror

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,13 +6,13 @@
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
-    /*visit the first node*/
-    func(tree->n);
-    /*traverse left nd right subtree*/
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	/*visit the first node*/
+	func(tree->n);
+	/*traverse left nd right subtree*/
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
