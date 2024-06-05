023e5ddd8145a69f96af8e2f02729b32a0e37381
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-06-05 13:34:45 +0100

    add files to check if a node is a leaf, traverse using preorder, and delete a tree

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - Goes through the binary tree using pre-order travasal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node.
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL || func == NULL)
+    {
+        return;
+    }
+    /*visit the first node*/
+    func(tree->n);
+    /*traverse left nd right subtree*/
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
+}
