a1aac2752bd862cc7e5053a32d44fcdcef56a511
Christiana Apinoko <christyapinoko@gmail.com>
2024-06-05 02:13:15 -0700

    fixed betty warnings

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-/***
+/**
  * binary_tree_insert_right - inserts a node as the right child of another node
  * @parent: Pointer to the node to insert the right child in
  * @value: Value to store in new node
@@ -7,27 +7,27 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = parent->right;
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = parent->right;
 
-    if (parent->right != NULL)
-    {
-        parent->right->parent = new_node;
-    }
-    parent->right = new_node;
+	if (parent->right != NULL)
+	{
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
